9f2a0cd863c6c9cd6597c52bb797d4f863023c03
Ricky Elrod <ricky@elrod.me>
2014-05-10 17:42:07 -0400

    Add some HTTPStatus instances
    Signed-off-by: Ricky Elrod <ricky@elrod.me>

--- core/src/main/scala/types/Status.scala
+++ core/src/main/scala/types/Status.scala
@@ -1,3 +1,26 @@
 package me.elrod.ph
 
+import scalaz._, Scalaz._
+
 case class HTTPStatus(code: Int, message: String)
+
+object HTTPStatus extends HTTPStatusInstances
+
+sealed trait HTTPStatusInstances {
+  implicit def HTTPStatusEqual: Equal[HTTPStatus] =
+    new Equal[HTTPStatus] {
+      def equal(a: HTTPStatus, b: HTTPStatus) =
+        Equal[Int].equal(a.code, b.code) && Equal[String].equal(a.message, b.message)
+    }
+
+  implicit def HTTPStatusOrder: Order[HTTPStatus] =
+    new Order[HTTPStatus] {
+      def order(a: HTTPStatus, b: HTTPStatus) =
+        Order[Int].order(a.code, b.code)
+    }
+
+  implicit def HTTPStatusShow: Show[HTTPStatus] =
+    new Show[HTTPStatus] {
+      override def shows(a: HTTPStatus) = s"${a.code} ${a.message}"
+    }
+}
