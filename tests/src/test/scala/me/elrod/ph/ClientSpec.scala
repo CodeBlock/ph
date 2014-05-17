94c8f6fe21939a462a8c9cb65cdf18b2598a23e2
Ricky Elrod <ricky@elrod.me>
2014-05-17 18:32:38 -0400

    [ph-test] remove unused imports

--- tests/src/test/scala/me/elrod/ph/ClientSpec.scala
+++ tests/src/test/scala/me/elrod/ph/ClientSpec.scala
@@ -3,20 +3,10 @@ package me.elrod.ph
 import me.elrod.ph.{ JavaClient => http }
 import me.elrod.ph.ResponseLenses._
 import monocle.syntax.lens._
-import org.scalacheck.Arbitrary
-import org.scalacheck.Arbitrary._
-import org.specs2.scalaz._
 import org.specs2.mutable._
 import purefn.bytestring.ByteString
-import purefn.bytestring.ByteString._
-import purefn.bytestring.io._
-import purefn.bytestring.syntax._
 import scalaz.effect.IO
-import scalaz.std.AllInstances._
-import scalaz.syntax.compose._
-import scalaz.syntax.equal._
 import scalaz.syntax.functor._
-import scalaz.syntax.id._
 
 class JavaClientSpec extends Specification {
   "JavaClient" should {
