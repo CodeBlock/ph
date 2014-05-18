fcb96d3bb4d1c37e2b7de280cc7cbbdda1ee2e8c
Ricky Elrod <ricky@elrod.me>
2014-05-18 05:36:35 -0400

    move get to the lens call

--- examples/src/main/scala/DisplayIP.scala
+++ examples/src/main/scala/DisplayIP.scala
@@ -11,6 +11,6 @@ object DisplayIP extends SafeApp {
   override def runc: IO[Unit] =
     for {
       ip <- http get "http://ipv4.da.gd/ip?text&strip" // Response[ByteString]
-      _ <- putStrLn((ip |-> body).get.toString)
+      _ <- putStrLn((ip |-> body get).toString)
     } yield ()
 }
